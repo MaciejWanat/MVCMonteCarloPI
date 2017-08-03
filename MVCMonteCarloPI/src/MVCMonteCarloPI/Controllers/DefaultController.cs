49e97728593494ac29b8fbcf864e2adb529d277b
Maciej Wanat <macio-111@o2.pl>
2017-08-03 07:59:47 +0200

    Generating image

--- MVCMonteCarloPI/src/MVCMonteCarloPI/Controllers/DefaultController.cs
+++ MVCMonteCarloPI/src/MVCMonteCarloPI/Controllers/DefaultController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using MVCMonteCarloPI.ViewModels;
 using MVCMonteCarloPI.Entities;
 
 namespace MVCMonteCarloPI.Controllers
--- MVCMonteCarloPI/src/MVCMonteCarloPI/Services/Logic.cs
+++ MVCMonteCarloPI/src/MVCMonteCarloPI/Services/Logic.cs
@@ -4,32 +4,15 @@ using System.Linq;
 using System.Threading.Tasks;
 using MVCMonteCarloPI.Entities;
 using ImageSharp;
+using System.IO;
 
 namespace MVCMonteCarloPI
 {
     public class Logic
     {
-
-        private static void DrawCircle(Image<Rgba32> image, double radius)
-        {
-            using(image)
-            {
-                for (double l = 0.0; l < 360.0; l += 0.1)
-                {
-                    double angle = l * System.Math.PI / 180;
-                    int x = (int)(radius + radius * System.Math.Cos(angle));
-                    int y = (int)(radius + radius * System.Math.Sin(angle));
-
-                    image[x, y] = Rgba32.Black;
-                }
-
-                image.Save("wwwroot/images/image.jpg");
-            }
-        }
-
+        
         public static double CalculatePI(int pointsAmount, int squareSide)
         {
-
             using (Image<Rgba32> image = new Image<Rgba32>(squareSide, squareSide))
             {
                 image.BackgroundColor(Rgba32.White);               
@@ -41,29 +24,54 @@ namespace MVCMonteCarloPI
             int total = 0;
             Random rnd = new Random();
 
-            while (i < pointsAmount)
+            using (Image<Rgba32> image = Image.Load("wwwroot/images/image.png"))
             {
-                int x = rnd.Next(0, squareSide); // points in rectangle
-                int y = rnd.Next(0, squareSide);
+                while (i < pointsAmount)
+                {
+                    int x = rnd.Next(0, squareSide); // points in rectangle
+                    int y = rnd.Next(0, squareSide);
 
-                float center = squareSide / 2;
+                    float center = squareSide / 2;
 
-                float Cx = x - center;
-                float Cy = y - center;
+                    float Cx = x - center;
+                    float Cy = y - center;
 
-                if (Cx * Cx + Cy * Cy <= squareSide / 2 * squareSide / 2) // Is the point in the circle?
-                {         
-                    ++numInCircle;
+                    if (Cx * Cx + Cy * Cy <= squareSide / 2 * squareSide / 2) // Is the point in the circle?
+                    {
+                        ++numInCircle;
+                        image[x, y] = Rgba32.Green;
+                    }
+                    else
+                        image[x, y] = Rgba32.Blue;
+
+                    ++total;
+                    i++;
                 }
-
-                ++total;
-                i++;
+                
+                image.Save("wwwroot/images/image.png");
             }
 
             double piApproximation = 4.0 * ((double)numInCircle / (double)total);
 
             return piApproximation;
 
+        }
+
+        private static void DrawCircle(Image<Rgba32> image, double radius)
+        {
+            using (image)
+            {
+                for (double l = 0.0; l < 360.0; l += 0.1)
+                {
+                    double angle = l * System.Math.PI / 180;
+                    int x = (int)(radius + radius * System.Math.Cos(angle));
+                    int y = (int)(radius + radius * System.Math.Sin(angle));
+
+                    image[x, y] = Rgba32.Red;
+                }
+
+                image.Save("wwwroot/images/image.png");
+            }
         }
     }
 }
